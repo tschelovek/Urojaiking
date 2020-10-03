141ff2bf4f80024e79d29d0b440b81c1ad2f619d
tschelovek <az18rg@gmail.com>
2020-10-03 20:45:10 +0300

    добавлены всплывающие подсказки

--- js/boxTarget.js
+++ js/boxTarget.js
@@ -1,21 +1,35 @@
 'use strict'
 
-const benefitsItem = document.querySelectorAll('.benefits__item');
+const benefitsItem = document.querySelectorAll('.displayChange');
 
 const deactivatedItem = () => {
-    benefitsItem.forEach(item => item.querySelector('.box2').classList.add('deactivated'))}
+    benefitsItem.forEach(item => item.querySelector('.displayChangeHided').classList.add('deactivated'))}
 
 deactivatedItem();
 
-benefitsItem.forEach((item) => item.addEventListener('mouseover', () => {
-    item.querySelector('.box1').classList.add('deactivated');
-    item.querySelector('.box2').classList.remove('deactivated');
-    })
+benefitsItem.forEach((item) => {
+        item.addEventListener('mouseover', () => {
+            item.querySelector('.displayChangeHided').classList.remove('deactivated');
+            if (item.querySelector('.displayChangeVisible') == null) {
+            } else {
+                item.querySelector('.displayChangeVisible').classList.add('deactivated');
+            }
+            if (item.querySelector('.marker') == null) {
+            } else {
+                item.querySelector('.marker').classList.add('highZ');
+            }
+        });
+    }
 );
 
 benefitsItem.forEach((item) => item.addEventListener('mouseout', () => {
     deactivatedItem()
-    item.querySelector('.box1').classList.remove('deactivated');
+    if (item.querySelector('.displayChangeVisible') == null) {
+    } else {
+    item.querySelector('.displayChangeVisible').classList.remove('deactivated');
+    }
+    if (item.querySelector('.marker') == null) {
+    } else {
+        item.querySelector('.marker').classList.remove('highZ');
+    }
 }));
-
-//TODO Сделать плавную анимацию перехода
